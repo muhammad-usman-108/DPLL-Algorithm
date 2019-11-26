0796ff9db44a24836b383c866351959f7b8fe488
Muhammad Usman <34769369+muhammad-usman-108@users.noreply.github.com>
2019-11-27 00:05:10 +0100

    Add files via upload

--- DPLL_Algo.py
+++ DPLL_Algo.py
@@ -27,17 +27,91 @@ def literal_values(CNF_Formula):
 
 def DPLL(CNF_Formula):
     trail.clear()
-    if not BCP():
-        return "UNSAT"
+    if not BCP(CNF_Formula):
+        #print("First")
+        return False
     while True:
         if not decide():
-            return "SAT"                                                                                                                                                                                                                                                                                                                                                                                                                                          n                                                                                                                                                
-        while not BCP():
+            return True
+        while not BCP(CNF_Formula):
             if not backtrack():
-                return "UNSAT"
+                #print("Second")
+                return False
 
-def BCP():
-    return False
+def BCP(CNF_Formula):
+
+    # Check IF: the clause have single literal ? THEN: Assign value such that the clause becomes TRUE
+    for clause in CNF_Formula:
+        if len(clause) == 1:
+            #print("Less than 1")
+            # IF literal is positive, which means it is without negation, so we need to assign TRUE value
+            if clause[0] > 0:
+                literal_list[clause[0]] = True
+                trail[clause[0]] = [True, True]
+            # ELSE literal is negative, which means it is with negation, so we need to assign FALSE value
+            else:
+                literal_list[abs(clause[0])] = False
+                trail[abs(clause[0])] = [False, True]
+
+    # Find the unit clause implying that a variable x must be set to value v ∈ {0,1}
+    # Check the clauses that have more than one literal
+    for clause in CNF_Formula:
+        falseCount = 0
+        unAssigned_variable = 0
+        if len(clause) != 1:
+            #print("Greater than 1", clause)
+            for literal in clause:
+                # Check IF literal value is assigned in stack 'trail'
+                if abs(literal) in trail.keys():
+                    if (literal > 0 and trail[abs(literal)][0]==True) or (literal < 0 and trail[abs(literal)][0]==False):
+                        break
+                    else:
+                        falseCount = falseCount + 1
+                # IF literal value is not assigned in stack, it means it is unassigned varaible
+                elif abs(literal) not in trail.keys():
+                    # IF: two unassigned variable found in clause THEN: it is Unresolved clause & we don't need to check it further
+                    if unAssigned_variable == 0:
+                        unAssigned_variable = literal
+                    else:
+                        break
+
+            if(falseCount == (len(clause)-1)):
+                if(unAssigned_variable > 0):
+                    literal_list[unAssigned_variable] = True
+                    trail[unAssigned_variable] = [True, True]
+                else:
+                    literal_list[abs(unAssigned_variable)] = False
+                    trail[abs(unAssigned_variable)] = [False, True]
+
+    #print("After Assign : ", trail)
+    # Check for unsatisfied clause
+    for clause in CNF_Formula:
+        #print(clause)
+        falseCount = 0
+        for literal in clause:
+            if abs(literal) in trail.keys():
+                if literal > 0 and trail[abs(literal)][0]== True:
+                    #print("Here 1 ", literal)
+                    break
+                elif literal < 0 and trail[abs(literal)][0]== False:
+                    #print("Here 2 ", literal)
+                    break
+                else:
+                    #print("Here 3 ", literal)
+                    falseCount = falseCount + 1
+
+        if falseCount == len(clause):
+            #print("BCP Return False")
+            return False
+    #print("BCP Return True")
+    return True
+
+
+
+
+
+
+
 
 def decide():
     # If all variables are assigned then return false
@@ -53,7 +127,9 @@ def decide():
         if literal_list[i] == None:
             literal_list[i] = False
             trail[i] = [False, False]
-    return True
+            #print("In decide : ", trail)
+            #print("In decide : ", literal_list)
+            return True
 
 def backtrack():
     while True:
@@ -69,9 +145,15 @@ def backtrack():
             trail[v[0]] = [not(v[1][0]), True]
             return True
 
-clauses = parse_dimacs("C:\\Users\\MUHAMMAD USMAN\\Downloads\\solver\\example-1.cnf")
-print(clauses)
+#clauses = parse_dimacs("C:\\Users\\MUHAMMAD USMAN\\Downloads\\benchmarks\\example-10.cnf")
+clauses = parse_dimacs(sys.argv[1])
+#print(clauses)
 literal_values(clauses)
-print( "List : ", literal_list)
-print(DPLL(clauses))
-print(trail)
+#print("List : ", literal_list)
+if DPLL(clauses):
+    print("sat")
+    exit(10)
+else:
+    print("unsat")
+    exit(20)
+#print("List : ", literal_list)
